9b5b3f1a5b259f583cc1a4bdb07e0a174d0a9c2a
bwb408114278 <408114278@qq.com>
2025-09-10 14:11:14 +0800

    feat(user): 同步SMTH用户

--- src/main/java/pn/torn/goldeneye/msg/strategy/faction/crime/OcBenefitRankStrategyImpl.java
+++ src/main/java/pn/torn/goldeneye/msg/strategy/faction/crime/OcBenefitRankStrategyImpl.java
@@ -2,13 +2,17 @@ package pn.torn.goldeneye.msg.strategy.faction.crime;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import pn.torn.goldeneye.base.exception.BizException;
 import pn.torn.goldeneye.constants.bot.BotCommands;
-import pn.torn.goldeneye.constants.torn.TornConstants;
 import pn.torn.goldeneye.msg.receive.QqRecMsgSender;
 import pn.torn.goldeneye.msg.send.param.QqMsgParam;
 import pn.torn.goldeneye.msg.strategy.PnMsgStrategy;
 import pn.torn.goldeneye.repository.dao.faction.oc.TornFactionOcBenefitDAO;
+import pn.torn.goldeneye.repository.dao.setting.TornSettingFactionDAO;
+import pn.torn.goldeneye.repository.dao.user.TornUserDAO;
 import pn.torn.goldeneye.repository.model.faction.oc.TornFactionOcBenefitRankDO;
+import pn.torn.goldeneye.repository.model.setting.TornSettingFactionDO;
+import pn.torn.goldeneye.repository.model.user.TornUserDO;
 import pn.torn.goldeneye.utils.TableImageUtils;
 
 import java.awt.*;
@@ -29,6 +33,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OcBenefitRankStrategyImpl extends PnMsgStrategy {
     private final TornFactionOcBenefitDAO benefitDao;
+    private final TornUserDAO userDao;
+    private final TornSettingFactionDAO settingFactionDao;
 
     @Override
     public String getCommand() {
@@ -42,22 +48,37 @@ public class OcBenefitRankStrategyImpl extends PnMsgStrategy {
 
     @Override
     public List<? extends QqMsgParam<?>> handle(long groupId, QqRecMsgSender sender, String msg) {
+        long userId;
+        try {
+            userId = super.getTornUserId(sender, msg);
+        } catch (BizException e) {
+            return super.buildTextMsg(e.getMsg());
+        }
+
+        TornUserDO user = userDao.getById(userId);
+        if (user == null) {
+            return super.buildTextMsg("金蝶不认识你哦");
+        }
+
         LocalDateTime fromDate = LocalDate.now().minusDays(LocalDate.now().getDayOfMonth() - 1L)
                 .atTime(0, 0, 0);
-        List<TornFactionOcBenefitRankDO> rankingList = benefitDao.queryBenefitRanking(TornConstants.FACTION_PN_ID,
+        List<TornFactionOcBenefitRankDO> rankingList = benefitDao.queryBenefitRanking(user.getFactionId(),
                 fromDate, LocalDateTime.now());
-        return super.buildImageMsg(buildRankingMsg(rankingList));
+        return super.buildImageMsg(buildRankingMsg(user.getFactionId(), rankingList));
     }
 
     /**
      * 构建OC收益排行榜表格
      */
-    private String buildRankingMsg(List<TornFactionOcBenefitRankDO> rankingList) {
+    private String buildRankingMsg(long factionId, List<TornFactionOcBenefitRankDO> rankingList) {
         DecimalFormat formatter = new DecimalFormat("#,###");
+        TornSettingFactionDO faction = settingFactionDao.getById(factionId);
+
         List<List<String>> tableData = new ArrayList<>();
         TableImageUtils.TableConfig tableConfig = new TableImageUtils.TableConfig();
 
-        tableData.add(List.of("PHN" + LocalDate.now().getMonthValue() + "月OC收益排行榜", "", "", ""));
+        tableData.add(List.of(faction.getFactionShortName() + "  " + LocalDate.now().getMonthValue() + "月OC收益排行榜",
+                "", "", ""));
         tableConfig.addMerge(0, 0, 1, 4);
         tableConfig.setCellStyle(0, 0, new TableImageUtils.CellStyle()
                 .setBgColor(Color.WHITE)
