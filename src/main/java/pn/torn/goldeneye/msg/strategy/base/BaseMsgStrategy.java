a0c96e64f97dd486a93ec75a2cc2a9e518abc508
bwb408114278 <408114278@qq.com>
2025-12-03 14:04:24 +0800

    feat(uesr): QQ绑定游戏用户

--- src/main/java/pn/torn/goldeneye/msg/strategy/base/BaseMsgStrategy.java
+++ src/main/java/pn/torn/goldeneye/msg/strategy/base/BaseMsgStrategy.java
@@ -7,10 +7,9 @@ import pn.torn.goldeneye.msg.receive.QqRecMsgSender;
 import pn.torn.goldeneye.msg.send.param.ImageQqMsg;
 import pn.torn.goldeneye.msg.send.param.QqMsgParam;
 import pn.torn.goldeneye.msg.send.param.TextQqMsg;
-import pn.torn.goldeneye.repository.dao.user.TornUserDAO;
 import pn.torn.goldeneye.repository.model.user.TornUserDO;
+import pn.torn.goldeneye.torn.manager.user.TornUserManager;
 import pn.torn.goldeneye.utils.NumberUtils;
-import pn.torn.goldeneye.utils.torn.TornUserUtils;
 
 import java.util.List;
 
@@ -18,12 +17,12 @@ import java.util.List;
  * 基础消息策略
  *
  * @author Bai
- * @version 0.3.0
+ * @version 0.4.0
  * @since 2025.07.24
  */
 public abstract class BaseMsgStrategy {
     @Resource
-    protected TornUserDAO userDao;
+    protected TornUserManager userManager;
 
     /**
      * 获取指令
@@ -77,23 +76,19 @@ public abstract class BaseMsgStrategy {
      * 根据消息和发送人获取用户ID
      */
     protected TornUserDO getTornUser(QqRecMsgSender sender, String msg) {
-        long userId;
+        TornUserDO user;
         if (StringUtils.hasText(msg)) {
             String[] msgArray = msg.split("#");
             if (msgArray.length < 1 || !NumberUtils.isLong(msgArray[0])) {
                 throw new BizException("参数有误");
             }
 
-            userId = Long.parseLong(msgArray[0]);
+            long userId = Long.parseLong(msgArray[0]);
+            user = userManager.getUserById(userId);
         } else {
-            userId = TornUserUtils.getUserIdFromSender(sender);
+            user = userManager.getUserByQq(sender.getUserId());
         }
 
-        if (userId == 0L) {
-            throw new BizException("金蝶不认识你哦");
-        }
-
-        TornUserDO user = userDao.getById(userId);
         if (user == null) {
             throw new BizException("金蝶不认识你哦");
         }
@@ -128,8 +123,7 @@ public abstract class BaseMsgStrategy {
      * 根据发送人获取帮派ID
      */
     protected long getTornFactionIdBySender(QqRecMsgSender sender) {
-        long userId = TornUserUtils.getUserIdFromSender(sender);
-        TornUserDO user = userDao.getById(userId);
+        TornUserDO user = getTornUser(sender, "");
         return user == null ? 0L : user.getFactionId();
     }
 }
